4699295b16b2c2c5ba3fe4e5609e09da9f14148e
QmelZ <59574967+QmelZ@users.noreply.github.com>
2020-11-07 14:54:57 +0300

    fix error reeeeeee
    im dumb

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ Vars.enableConsole = true;
 require("content/unlock");
 require("content/blocks");
 require("content/turrets");
-require("/content/cursed");
+require("content/cursed");
 
 // Enable infniteResources upon a map load
 Events.on(WorldLoadEvent, e => {Vars.state.rules.infiniteResources = true});
