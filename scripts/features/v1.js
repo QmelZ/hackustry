8b406541890f331f1b4970f95771e81e02be49b2
QmelZ <59574967+QmelZ@users.noreply.github.com>
2021-09-24 08:32:08 +0300

    fix desync part 1

--- scripts/features/v1.js
+++ scripts/features/v1.js
@@ -1,12 +1,16 @@
-const hconv = extend(Conveyor, "hackusated-conveyor", {
-    localizedName: "Hackusated Conveyor",
-    description: "unit railgun",
-    category: Category.distribution,
-    buildVisibility: BuildVisibility.hidden,
-    inEditor: false,
-    health: 99999,
-    speed: 99999,
-    alwaysUnlocked: true
+let hconv;
+
+Events.on(ContentInitEvent, () => {
+    hconv = extend(Conveyor, "hackusated-conveyor", {
+        localizedName: "Hackusated Conveyor",
+        description: "unit railgun",
+        category: Category.distribution,
+        buildVisibility: BuildVisibility.hidden,
+        inEditor: false,
+        health: 99999,
+        speed: 99999,
+        alwaysUnlocked: true
+    });
 });
 
 module.exports = (add) => {
